2f55117ea4ddc52b158c187fdddd1c4ef6a45c57
Rikker Ferenc Mate <rikker.ferenc@BOLYAI.LOCAL>
2026-01-20 11:34:46 +0100

    kesz mostmar

--- kitero/hf.js
+++ kitero/hf.js
@@ -1,4 +1,5 @@
-class Tanyer
+//Class
+class Tanyer1
 {
     constructor (color, size)
     {
@@ -7,17 +8,42 @@ class Tanyer
     }
 }
 
-function Pohar(color)
+class Pohar1
 {
-    this.color = color;
+    constructor(color)
+    {
+        this.color = color;
+    }
 }
 
-const tanyer1 = new Tanyer("blue", "small");
-const tanyer2 = new Tanyer("red", "small");
-const tanyer3 = new Tanyer("green", "big");
-const pohar = new Pohar("black");
+const tanyer1 = new Tanyer1("blue", "small");
+const tanyer2 = new Tanyer1("red", "small");
+const tanyer3 = new Tanyer1("green", "big");
+const pohar1 = new Pohar1("black");
 
 console.log(tanyer1);
 console.log(tanyer2);
 console.log(tanyer3);
-console.log(pohar);
+console.log(pohar1);
+
+//Function
+function Tanyer2(color, size)
+{
+    this.color = color;
+    this.size = size;
+}
+
+function Pohar2(color)
+{
+    this.color = color;
+}
+
+const tanyer4 = new Tanyer2("yellow", "big");
+const tanyer5 = new Tanyer2("pink", "small");
+const tanyer6 = new Tanyer2("magenta", "small");
+const pohar2 = new Pohar2("orange");
+
+console.log(tanyer4);
+console.log(tanyer5);
+console.log(tanyer6);
+console.log(pohar2);
