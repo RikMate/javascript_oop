30c2a2e03629a8b4c0d2f0b235a16bd0fc6bfb1b
Rikker Ferenc Mate <rikker.ferenc@BOLYAI.LOCAL>
2026-04-24 11:22:13 +0200

    jegyzet 1?

--- auther_manager/navigationBar.js
+++ auther_manager/navigationBar.js
@@ -1,17 +1,17 @@
 import { createRadioButton } from "./gomszab.min.js";
 import { ViewElement } from "./viewElement.js";
 
-class NavigationBar extends ViewElement
+class NavigationBar extends ViewElement // navigationBar osztály definíciója
 {
     /**@type {ViewElement[]} */
-    #viewElementList;
-     constructor()
+    #viewElementList; // privát tulajdonság, ami tartalmazza a megjelenítendő viewelement leszármazottakat (táblázat, form, importexport)
+     constructor() // konstruktor definíció
      {
-        super('navbar');
-        this.#viewElementList = [];
-        this.div.addEventListener("change", (e) => {
-            const radioButtonValue = e.target.value;
-            this.activate(radioButtonValue);
+        super('navbar'); // meghívjuk a szülőosztály konstruktorát
+        this.#viewElementList = []; // inicializáljuk a viewelementlistet egy üres tömbbel
+        this.div.addEventListener("change", (e) => { // feliratkozunk a div change eseményére (mivel a div radiógombokat for tartalmazni, ezért tudjuk figyelni a divnél, hogy melyik rádiógomb van kijelölve)
+            const radioButtonValue = e.target.value; // elkérjük a target value értékét
+            this.activate(radioButtonValue); // meghívjuk az activate függvény a kiválasztott rádiógomb értékével (a viewelement azonosítói lehetnek lásd: addViewElement)
         })
      }
 
@@ -19,23 +19,23 @@ class NavigationBar extends ViewElement
       * @param {string} label 
       * @param {viewElement} viewElement 
       */
-     addViewElement(label, viewElement)
+     addViewElement(label, viewElement) // navigationbae példányának definiál egy függvényt
      {
-        this.#viewElementList.push(viewElement);
-        const div = createRadioButton({id: viewElement.id, name: this.id, label});
-        this.div.appendChild(div);
+        this.#viewElementList.push(viewElement); // bemeneti viewelement hozzáadjuk a viewElementListhez
+        const div = createRadioButton({id: viewElement.id, name: this.id, label}); 
+        this.div.appendChild(div); // hozzáfüzzük a divheza rádiógomb kreálás visszatérési értékét (this.div lásd: viewelement osztály definiciója)
      }
      /**
       * @override
       * @param {string} value 
       */
-     activate(value)
+     activate(value) // A szülő osztály definiál egy activate függvényt lásd: ViewElement.activate), de a navigác
      {
-        for(const viewElement of this.#viewElementList)
+        for(const viewElement of this.#viewElementList) // végigiterálunk a viewelementlist (table, form, és importexportot tartalmazza)
         {
-            viewElement.activate(value);
+            viewElement.activate(value); //  meghívjuk az activate függvényét minden viewelementként
         }
-        this.div.querySelector(`#${value}`).checked = true;
+        this.div.querySelector(`#${value}`).checked = true; // a diven belül lekérjük a bemeneti paraméterrel megegyező id jó element
      }
 }
 
